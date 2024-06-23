3eb8927a4c787e89e5e60b2e036a403f7251a141
Zack Mahand <78578405+zmahand@users.noreply.github.com>
2024-06-22 20:58:40 -0500

    feat: write logic to get computer choice

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+let computerChoice;
+
+function getComputerChoice() {
+    let number = Math.random();
+    if (number < 0.33) {
+        computerChoice = 'rock';
+    } else if (number < 0.66) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    }
+    return computerChoice;
+}
+
