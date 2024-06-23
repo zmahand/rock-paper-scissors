91245eba097527a43ac52ae87dc4bbe9d505028b
Zack Mahand <78578405+zmahand@users.noreply.github.com>
2024-06-22 21:19:12 -0500

    feat: write logic to play a single round.

--- app.js
+++ app.js
@@ -21,3 +21,41 @@ function getHumanChoice () {
     return humanChoice;
 }
 
+function playRound (computerChoice, humanChoice) {
+    if (computerChoice === 'rock') {
+        if (humanChoice === 'rock') {
+            console.log("It's a tie!");
+        } else if (humanChoice === 'paper') {
+            console.log('You win! Paper beats rock!');
+            humanScore += 1;
+        } else {
+            console.log('You lose! Rock beats scissors!');
+            computerScore += 1;
+        }
+    } else if (computerChoice === 'paper') {
+        if (humanChoice === 'rock') {
+            console.log('You lose! Paper beats rock!');
+            computerScore += 1;
+        } else if (humanChoice === 'paper') {
+            console.log("It's a tie!");
+        } else {
+            console.log('You win! Scissors beats paper!');
+            humanScore += 1;
+        }
+    } else {
+        if (humanChoice === 'rock') {
+            console.log('You win! Rock beats scissors!');
+            humanScore +=1;
+        } else if (humanChoice === 'paper') {
+            console.log('You lose! Scissors beats paper!');
+            computerScore += 1;
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(computerSelection, humanSelection)
